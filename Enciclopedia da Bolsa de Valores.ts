44ca02fe0b5b7a2e3125d8811261ae5f00a8e8cb
renanpyd <61557166+renanpyd@users.noreply.github.com>
2022-03-11 16:29:10 -0300

    Add files via upload

---
+++ Enciclopedia da Bolsa de Valores.ts
@@ -0,0 +1,133 @@
+ENCICLOPÉDIA DA BOLSA DE VALORES
+================================
+
+A 
+	Ações: Título emitido pela sociedade anônima, representativo do valor da fração em que é dividido o Capital Social.
+	Ações Ordinárias: Possuem direito a voto nas assembléias da empresa. 
+	Ações Preferenciais: Tem preferência na distribuição de dividendos e não possuem direito a voto nas assembléias. (tende a render mais, por não ter direito a voto nas assembléias)
+	ADR’s: American Depositary Receipts em síntese são ações brasileiras negociadas nas bolsas americanas.
+	ANBID: Associação Nacional dos Bancos de Investimento.
+	Análise Fundamentalista: Baseada no estudo dos fundamentos econômicos financeiros da empresa. 
+	Análise Técnica ou gráfica: Baseada no estudo de gráficos de comportamento das negociações dos ativos. 
+	Arbitrador: Participante do mercado que em função da sua atuação assegura que o preço negociado a futuro seja sempre justo.
+	Ativo Objeto: Consiste no ativo a que uma determinada opção se refere.
+B 
+	Bacen: Banco Central do Brasil 
+	Balança Comercial: Mede o resultado das transações de bens entre o Brasil e o resto do mundo. (Resultado das exportações menos as importações nos fornece o saldo da balança comercial)
+	Balança de Serviços: Mede o resultado das transações de serviços entre o Brasil e o resto do mundo. (Nessa conta é computado o saldo entre nossas remessas de lucros, juros, royalties, seguros, viagens internacionais, etc; e aquilo que recebemos do exterior nessas mesmas categorias)
+	Bens de capital: São os bens que servem para a produção de outros bens, tais como máquinas e equipamentos.
+	Best-Effort Underwriting: É o lançamento de ações no qual a instituição financeira que subscreve os ativos assume apenas o compromisso de fazer o melhor esforço para colocar o máximo de uma emissão junto a sua clientela, nas melhores condições possíveis e com período de tempo determinado.
+	Block Trade: Oferta de grande lote de ações antigas (em posse de algum grande acionista) com colocação junto ao público através das bolsas de valores/ e ou mercado de balcão. 
+	Blue-Chips: Termo utilizado nas bolsas de valores para designar as principais ações, no Brasil podemos citar: Petrobras, Vale e Gerdau.
+	BNDES: Banco Nacional de Desenvolvimento Econômico e Social. 
+	Bolsa de Valores: Associação civil, sem fins lucrativos ou sociedades anônimas, possui autonomia financeira, patrimonial e administrativa.
+	Bonificação: Ações novas, emitidas em função de aumento de capital decorrente da incorporação de reservas que são distribuídas gratuitamente aos sócios. 
+	Bônus de Subscrição: Título emitido por uma Sociedade Anônima, que confere ao seu titular, nas condições constantes no certificado, direto de subscrever ações, que será exercido contra apresentação do bônus a companhia e pagamento do preço de emissão da ação.
+	Book Building: Construção do livro, sistema usado para emissão de novas ações para que o mercado defina o preço de abertura. 
+	Bovespa: Bolsa de valores de São Paulo no Brasil.
+C 	
+	Câmbio: É o mercado onde ocorre a negociação de moedas estrangeiras entre as instituições ou pessoas interessadas em movimentar essas moedas.
+	CBLC: Companhia Brasileira de Liquidação e Custódia 
+	CDB: Certificado de Depósito Bancário 
+	CDI: Certificado de Depósito Interfinanceiro 
+	CETIP: Câmara de Custódia e Liquidação 
+	Clube de Investimento: Instrumento de participação dos pequenos e médios investidores no mercado de ações.
+	CMN: Conselho Monetário Nacional
+	Commercial Paper: Notas Promissórias Comerciais, equivalente a debêntures de curto prazo.
+	Companhia de Capital aberto: Registrada na CVM e autorizada a distribuir valores mobiliários de sua emissão junto ao público.
+	Companhia de Capital fechado: Sem registro na CVM, os títulos de sua emissão só podem ser objetos de transações privadas.
+	CVM: Comissão de Valores Mobiliários
+D 
+	Day Trade: Operação de compra e venda de uma mesma quantidade de ações de uma mesma empresa, realizada no mesmo dia pelo mesmo investidor, através da mesma corretora e utilizando o mesmo agente de compensação.
+	DAX: Bolsa de valores da Alemanha
+	Debêntures: São títulos de crédito de longo prazo emitidos por sociedades anônimas.
+	Desdobramento (split): Ações novas distribuídas gratuitamente aos acionistas, nesse caso não há alteração no capital social.
+	Dividendos: Parte do lucro da empresa, já descontado o imposto de renda que é distribuída a seus acionistas.
+E 	
+	Especulador: É aquele que negocia tendência de preço, dando liquidez as operações no mercado.
+F 
+	FED: Banco Central dos Estados Unidos da América.
+	Financiador: Investidor que executa operações de renda fixa, combinando compras, no mercado a vista com vendas de contratos no mercado futuro, fixando seu ganho nominal.
+	FTSE All Share: Bolsa de valores de Londres na Inglaterra.
+G 
+	GAP: Em inglês significa abertura, fenda, lacuna. Na bolsa de valores essa palavra é utilizada para caracterizar a diferença de preços que um ativo é negociado em períodos diferentes, por exemplo: determinado ativo fechou o pregão a R$ 40,00 e no pregão seguinte tem sua abertura cotada a R$ 42,00, esse espaço entre as duas cotações caracteriza um gap.
+H
+	Hedge: Significa ato de se defender, proteger-se de risco ou prejuízo, por meio de arranjos compensatórios no outro lado. 
+	Hedger: Principal função dos mercados futuros é permitir a proteção contra riscos de preços ou realizar o hedge.
+	Home Broker: Sistema de envio de ordens via internet para a bolsa de valores, utilizada pelos investidores para realizarem suas operações em qualquer lugar do mundo.
+I 	
+	IGBC: Bolsa de valores da Colômbia 
+	IGBVL,ISBVL: Bolsa de valores de Lima no Peru.
+	IGP-DI: Índice Geral de Preços – Disponibilidade Interna, é calculado pela Fundação Getúlio Vargas (FGV), é formada por três índices: 
+		Índice de Preços por Atacado (IPA) 
+		Índice de Preços ao Consumidor (IPC) 
+		Índice Nacional da Construção Civil.
+	IGP-M: Índice Geral de Preços do Mercado é calculado pela Fundação Getúlio Vargas (FGV), sua coleta é feita entre o 21º dia do mês anterior e o 20º do mês em referência. É utilizado como parâmetro de inflação do mercado financeiro.
+	IGPA: Bolsa de Santiago no Chile
+	IPCA: Índice de preços ao Consumidor Amplo (IPCA), calculado pelo IBGE desde 1980, utilizado pelo Banco Central do Brasil como parâmetro para o sistema de metas inflacionárias desde julho/1999. Apuração mensal, seu período de coleta abrande do primeiro ao último dia do mês de referência.
+	IPCompMX: Bolsa de valores do México.
+	Investidor qualificado: São investidores PF ou PJ que possuam investimentos financeiros em valor superior a R$ 300 mil.
+J	
+	Joint-Venture: Associação de empresas para o desenvolvimento e execução de um projeto específico.
+	Juros sobre capital próprio: Recursos que são pagos aos acionistas pelo aporte de capital, os acionistas ao receberem esse recurso pagam 15% de imposto de renda.
+L 	
+	LC: Letra de Câmbio 
+	LFT: Letra Financeira do Tesouro 
+	LH: Letra Hipotecária 
+	LI: Letra Imobiliária 
+	Libor: London Interbank Offered Rate, a Libor é normalmente utilizada no Brasil nas operações financeiras internacionais e nas operações de comércio internacional. 
+	LTN: Letra do Tesouro Nacional
+M 	
+	Mercado de opções: Mercado onde não se negociam ativos financeiros ou commodities e sim direitos de compra ou de venda de um lote de ações, ativos financeiros ou commodities, com preços e prazos de exercício preestabelecidos contratualmente.
+	Mercado a termo: Nesse mercado compra-se ou vende-se um ativo com vencimento em determinada data futura, por um preço previamente estabelecido em mercado.
+	Mercado a vista: O mercado a vista é onde se realizam operações de compra e venda de ações emitidas pelas empresas abertas registradas em Bolsa.
+	MerVal:Bolsa de valores de Buenos Aires na Argentina.
+	MIB: Bolsa de valores da Itália
+N 	
+	Nasdaq: Bolsa eletrônica americana.
+	NTN: Nota do Tesouro Nacional
+	NYSE: New York Stock Exchange, a maior bolsa de valores do mundo situada na cidade de Nova Iorque.
+O 	
+	Open Market: É o mercado secundário privativo das instituições financeiras, onde se negociam títulos já emitidos anteriormente e onde ocorre a troca de recursos bancários lastreados em títulos públicos federais.
+	Opções Americanas: Tipo de opção que permite seu exercício a qualquer momento até a data de vencimento previamente estabelecida.
+	Opção de Compra (call): Concede o direito de comprar uma quantidade específica de um ativo objeto, a um determinado preço de exercício e em prazo determinado.
+	Opções Européias: Tipo de opção que permite seu exercício somente na data previamente estabelecida.
+	Opção de Venda (put): Concede o direito de vender uma quantidade específica de um ativo objeto, a um determinado preço de exercício e em prazo determinado.
+P 	
+	PIB: Produto Interno Bruno, mede o total de produção do país ao longo de um determinado período. Calculado pelo IBGE e divulgado trimestralmente.
+	Posição simples: Posição comprada ou vendida de um ativo. 
+	Posição sintética: Combinação de dois ou mais futuros na compra ou venda. 
+	Preço de Emissão: Preço de emissão de novos ativos no mercado, é determinado previamente pela empresa emissora ou então através do procedimento de “book building”. 
+	Preço de Exercício: É o preço em que se dará o exercício do contrato futuro (opção).
+	Prêmio da opção: Preço negociado para adquirir o direito de exercício da opção. 
+	Prime Rate: Taxa de juros básicos utilizada por bancos comerciais norte-americanos em empréstimos para clientes preferenciais.
+	Produção Industrial – IBGE: Mede o total da produção física de bens em determinado mês em diversas regiões do país. Abrange bens de consumo (duráveis e não duráveis), bens intermediários e bens de capital.
+R 	
+	Resultado Nominal das contas públicas: É a soma do resultado primário com o resultado do pagamento e recebimento de juros.
+	Resultado Primário das contas públicas: Resultado das contas do Governo Federal ( Tesouro Nacional, Banco Central e Previdência), o resultado primário serve para avaliar o equilíbrio fiscal de um país.
+	Risco não sistêmico: Risco ligado a fatores que afetam negativamente uma empresa ou um setor em particular.
+	Risco sistêmico: Risco ligado a fatores que afetam negativamente o mercado com um todo.
+S 	
+	Saldo das Transações Correntes (Conta-Corrente): Mede o resultado de todas as nossas transações com o exterior, tanto de bens quanto de serviços. 
+	SELIC: Sistema Especial de Liquidação e Custódia.
+	Série das opções: São opções do mesmo tipo (compra ou venda) referentes ao mesmo ativo objeto, tendo a mesma data de vencimento e o mesmo preço de exercício.
+	Sociedade Anônima(S.A.): Modelo jurídico que visa permitir a operacionalização de uma companhia, sem maiores responsabilidades para seus sócios do que o capital investido.
+	Spread- Em Inglês a palavra Spread significa diferença. Em opções é a diferença de compra de uma opção e o preço de venda de outra opção.
+	SSE Composite: Bolsa de valores de Shangai 
+	Stop: Tipo de ordem que busca evitar uma perda ou realizar um lucro quando o ativo estiver cotado a determinado valor.
+	Subscrição Particular: Ações novas que são emitidas pela companhia e vendidas preferencialmente aos atuais acionistas.
+	Subscrição Pública: Ações novas que são emitidas pela companhia e vendidas para qualquer investidor nelas interessado.
+	Swap: Derivativo onde duas partes pactuam uma troca de indexadores por um determinado tempo e sobre um montante previamente acordado. É um instrumento de hedging e gestão de risco, não tendo como finalidade o levantamento de fundos.
+	S&P 500: Bolsa de valores americana que reúne as 500 principais empresas dos Estados Unidos da América.
+T 	
+	Taxa de Risco do País: Indicador de uma taxa de juros anual linear calculada com base nos níveis de remuneração praticados no mercado secundário de títulos da dívida externa pública de alguns países emergentes. Usualmente é expressa em pontos base ( cada ponto base equivale a 0,01%). 
+	TBF: Taxa Básica Financeira.
+	TJLP: Taxa de Juros de Longo Prazo. 
+	Topix: Bolsa de valres de Tóquio no Japão. 
+	TR: Taxa referencial de juros calculada e divulgada diariamente pelo Banco Central do Brasil, é a taxa que define o rendimento da Caderneta de Poupança e dos empréstimos do Crédito Rural e do SFH.
+U 
+	Underwriter: São instituições financeiras especializadas em operações de subscrições de ações ou debêntures, junto ao público, em geral são bancos de investimentos, sociedades corretoras ou sociedades distribuidoras.
+	Underwriting de ações ou debêntures: É o processo de lançamento de ações ou debêntures para distribuição pública. 
+	Underwriter Firme: É o processo de lançamento no qual a instituição financeira que subscreve os ativos, assegura a garantia firme de colocação, assumindo o compromisso de adquirir para carteira própria qualquer quantidade de títulos eventualmente não colocada junto aos investidores individuais e institucionais.
+V 
+	Vencimento de opção: É o último dia que um contrato futuro pode ser encerrado por meio da realização de uma operação oposta no próprio Mercado Futuro.
